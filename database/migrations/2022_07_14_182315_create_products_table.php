78ccc5809c0a642aba93ad6975e7a020549f505e
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-14 20:50:21 +0200

    999

--- database/migrations/2022_07_14_182315_create_products_table.php
+++ database/migrations/2022_07_14_182315_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 100);
+            $table->string('title', 999);
             $table->text('description');
             $table->string('thumb')->nullable();
             $table->decimal('price', 5);
