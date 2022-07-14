7fa941f4a4eeb81814a4ac965aad35646dc40e3e
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-07-14 22:24:23 +0200

    progressi product

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fumetti = config('fumetti');
+
+        foreach ($fumetti as $fumetto) {
+            
+            $newProduct = new Product();
+            
+            $newProduct->title = $fumetto['title'];
+            $newProduct->description = $fumetto['description'];
+            $newProduct->thumb = $fumetto['thumb']; 
+            $newProduct->price = $fumetto['price'];
+            $newProduct->series = $fumetto['series'];
+            $newProduct->sale_data = $fumetto['sale_date'];
+            $newProduct->type = $fumetto['type'];
+            
+            $newProduct->save();
+        }
+    }
+    
+}
